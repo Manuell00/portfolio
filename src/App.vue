41dff580e040ae76f1737d7bfa6c18a91db0ae57
Manuell00 <manuell.caselli@gmail.com>
2023-09-01 15:52:52 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<script>
+
+// Importo lo STORE
+import { store } from './store.js';
+// Importo AXIOS 
+import axios from 'axios';
+
+// Importo i COMPONENTI
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
+
+
+// Inserisco l'EXPORT
+export default {
+  components: {
+    AppLoader,
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+
+  // Inserisco i dati
+  data() {
+    return {
+      store,
+    }
+  },
+
+  // Dovrò inserire i metodi
+  methods: {
+
+  },
+
+  // CREATED viene utilizzato al posto di mounted perchè svolge l'attività quando viene creata l'app
+  created() {
+  }
+
+}
+
+
+</script>
+
+
+
+<!-- TEMPLATE -->
+<template>
+  <!-- Inserisco l'header e il suo componente -->
+  <header>
+    <AppHeader />
+  </header>
+
+  <!-- Inserisco il main e il suo componente -->
+  <main>
+    <AppMain />
+  </main>
+
+  <!-- Inserisco il footer e il suo componente -->
+  <footer>
+    <AppFooter />
+  </footer>
+</template>
+
+
+
+<!-- Inserisco lo style -->
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use './styles/partials/variables.scss' as *;
+</style>
