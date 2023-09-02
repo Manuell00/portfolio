f6c31257ee137a3e1047722ae835c9f0e425305c
Manuell00 <manuell.caselli@gmail.com>
2023-09-02 16:39:40 +0200

    miglioramento codice

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -60,7 +60,32 @@ export default {
 
             <!-- Secondo contenuto (Sticker) -->
             <div class="emoj">
+
+                <!-- Prima svg -->
+                <svg class="mini-img" width="800px" height="800px" viewBox="0 0 24 24" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9 6L3.40208 11.1314C3.1859 11.3296 3.1859 11.6704 3.40208 11.8686L9 17" stroke="#000000"
+                        stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M14 17L19.5979 11.8686C19.8141 11.6704 19.8141 11.3296 19.5979 11.1314L14 6" stroke="#000000"
+                        stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+
+                <!-- Immagine -->
                 <img src="../../computers.svg" alt="">
+
+                <!-- Seconda svg -->
+                <svg class="mini-img" width="800px" height="800px" viewBox="0 0 24 24" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M7.90451 6.92144C8.41341 6.45495 8.44779 5.66424 7.9813 5.15534C7.51481 4.64645 6.7241 4.61207 6.2152 5.07856L7.90451 6.92144ZM1.46194 11.1314L2.3066 12.0529L2.3066 12.0529L1.46194 11.1314ZM1.46194 11.8686L2.3066 10.9471L2.3066 10.9471L1.46194 11.8686ZM6.2152 17.9214C6.7241 18.3879 7.51481 18.3536 7.9813 17.8447C8.44779 17.3358 8.41341 16.545 7.90451 16.0786L6.2152 17.9214ZM6.2152 5.07856L0.617287 10.21L2.3066 12.0529L7.90451 6.92144L6.2152 5.07856ZM0.617287 12.79L6.2152 17.9214L7.90451 16.0786L2.3066 10.9471L0.617287 12.79ZM0.617287 10.21C-0.139356 10.9036 -0.139356 12.0964 0.617287 12.79L2.3066 10.9471C2.63087 11.2444 2.63087 11.7556 2.3066 12.0529L0.617287 10.21Z"
+                        fill="#000000" />
+                    <path
+                        d="M16.0557 16.0786C15.5468 16.545 15.5125 17.3358 15.9789 17.8447C16.4454 18.3536 17.2361 18.3879 17.745 17.9214L16.0557 16.0786ZM22.4983 11.8686L21.6537 10.9471L21.6537 10.9471L22.4983 11.8686ZM22.4983 11.1314L21.6537 12.0529L21.6537 12.0529L22.4983 11.1314ZM17.745 5.07856C17.2361 4.61207 16.4454 4.64645 15.979 5.15534C15.5125 5.66424 15.5468 6.45495 16.0557 6.92144L17.745 5.07856ZM17.745 17.9214L23.343 12.79L21.6537 10.9471L16.0557 16.0786L17.745 17.9214ZM23.343 10.21L17.745 5.07856L16.0557 6.92144L21.6537 12.0529L23.343 10.21ZM23.343 12.79C24.0996 12.0964 24.0996 10.9036 23.343 10.21L21.6537 12.0529C21.3294 11.7556 21.3294 11.2444 21.6537 10.9471L23.343 12.79Z"
+                        fill="#000000" />
+                    <path
+                        d="M15.2127 3.80317C15.3801 3.13343 14.9729 2.45476 14.3032 2.28732C13.6334 2.11989 12.9548 2.52709 12.7873 3.19683L15.2127 3.80317ZM8.78732 19.1968C8.61989 19.8666 9.02709 20.5452 9.69683 20.7127C10.3666 20.8801 11.0452 20.4729 11.2127 19.8032L8.78732 19.1968ZM12.7873 3.19683L8.78732 19.1968L11.2127 19.8032L15.2127 3.80317L12.7873 3.19683Z"
+                        fill="#000000" />
+                </svg>
             </div>
 
         </div>
@@ -112,6 +137,15 @@ export default {
                 </div>
 
             </div>
+        </div>
+
+        <!-- MY WORKS -->
+        <div class="work">
+            <div class="work-header">
+
+
+            </div>
+
         </div>
 
     </div>
@@ -220,10 +254,15 @@ export default {
             display: flex;
             align-items: center;
             justify-content: center;
-            flex-direction: column;
+            flex-direction: row;
+
+            .mini-img {
+                max-width: 200px; // Questo garantisce che le immagini non siano più grandi del loro contenitore
+                height: auto;
+            }
 
             img {
-                width: 400px;
+                max-width: 500px; // Questo garantisce che le immagini non siano più grandi del loro contenitore
                 height: auto;
             }
         }
@@ -239,10 +278,6 @@ export default {
         background: linear-gradient(to bottom, black, $violet );
         background-size: 200% 200%;
 
-        &:hover {
-            animation: GradientAnimation 5s infinite;
-        }
-
         .tooling-title {
             align-items: center;
             background: #12141d;
@@ -253,9 +288,10 @@ export default {
             display: flex;
             justify-content: center;
             margin-bottom: 5rem;
-            padding: .8rem 2rem;
+            padding: 0 3rem;
 
             h2 {
+                font-size: 3.5rem;
                 color: $violet;
                 font-family: 'CYBORG', sans-serif;
                 font-weight: 900;
@@ -292,12 +328,35 @@ export default {
             }
         }
     }
+
+    // WORKS
+    .work .work-header {
+        align-items: flex-start;
+        display: flex;
+        justify-content: flex-start;
+        padding-left: 5rem;
+    }
 }
 
 
 // MEDIA QUERY
 
-@media (max-width: 1200px) {
+@media (max-width: 1700px) {
+
+    .container-fluid {
+        .emoj {
+            .mini-img {
+                width: 150px;
+            }
+
+            img {
+                width: 350px; // Slightly reduce the image width for large devices
+            }
+        }
+    }
+}
+
+@media (max-width: 1300px) {
 
     // For large devices like laptops
     .container-fluid {
@@ -306,8 +365,15 @@ export default {
                 width: 70%; // Increase width for more space
             }
 
-            .emoj img {
-                width: 300px; // Slightly reduce the image width for large devices
+
+            .emoj {
+                .mini-img {
+                    width: 100px;
+                }
+
+                img {
+                    width: 300px; // Slightly reduce the image width for large devices
+                }
             }
         }
     }
@@ -335,6 +401,10 @@ export default {
                 .emoj {
                     width: 100%; // Utilize full width
                     padding: 20px 0; // Add some vertical padding
+
+                    .mini-img {
+                        width: 80px;
+                    }
 
                     img {
                         width: 250px; // Reduce the image width for tablets
@@ -369,8 +439,14 @@ export default {
                 }
             }
 
-            .emoj img {
-                width: 200px; // Further reduce the image width for mobile phones
+            .emoj {
+                .mini-img {
+                    width: 50px;
+                }
+
+                img {
+                    width: 200px; // Further reduce the image width for mobile phones
+                }
             }
         }
     }
